f3aaaafa052f55e05e7828e889119e4fa255f6e0
graht <430702238@qq.com>
2024-09-29 15:09:27 +0800

    Update

---
+++ src/main/java/cn/graht/springbootinit/aop/AuthInterceptor.java
@@ -0,0 +1,46 @@
+package cn.graht.springbootinit.aop;
+
+import cn.graht.springbootinit.annotation.AuthCheck;
+import cn.graht.springbootinit.commons.ErrorCode;
+import cn.graht.springbootinit.exception.BusinessException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 权限校验 AOP
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Aspect
+@Component
+public class AuthInterceptor {
+
+
+
+    /**
+     * 执行拦截
+     *
+     * @param joinPoint
+     * @param authCheck
+     * @return
+     */
+    @Around("@annotation(authCheck)")
+    public Object doInterceptor(ProceedingJoinPoint joinPoint, AuthCheck authCheck) throws Throwable {
+        String mustRole = authCheck.mustRole();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        // 当前登录用户
+        //todo  进行鉴权
+        //通过权限校验，放行
+        return joinPoint.proceed();
+    }
+}
+
--- src/main/java/cn/graht/springbootinit/configuration/AppConf.java
+++ src/main/java/cn/graht/springbootinit/configuration/AppConf.java
@@ -1,5 +1,4 @@
 package cn.graht.springbootinit.configuration;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,21 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class AppConf implements WebMvcConfigurer {
-    static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE","OPTIONS"};
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 所有的当前站点的请求地址，都支持跨域访问。
-                .allowedOrigins("http://localhost:5173") // 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
-                .allowCredentials(true) // 是否支持跨域用户凭证
-                .allowedMethods(ORIGINS) // 当前站点支持的跨域请求类型是什么
-                .maxAge(3600); // 超时时长设置为1小时。 时间单位是秒。
-    }
 
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        return paginationInterceptor;
-    }
     @Bean(name = "encodedRedisTemplate")
     public RedisTemplate<String,Object> encodedRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate redisTemplate = new RedisTemplate();
